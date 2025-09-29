ef6a27ce3861f249d3578005e9de7584c3fdc6ba
Abdalr7man <Eng.Abdalrhman.Abdalmonem@gmail.com>
2025-09-29 16:55:08 +0300

    Done

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # GitHub-User-Activity
 
-very useful project for learning the dealing with APIs, Cli arguments, JSON and show data
-
-- <https://roadmap.sh/projects/github-user-activity>
+very useful project for learning the dealing with APIs, Cli arguments, JSON and show data without using any external libraries.
 
 You can learn more about the GitHub API here.
 
-- <https://docs.github.com/en/rest/activity/events?apiVersion=2022-11-28>
+- [REST API endpoints for events](https://docs.github.com/en/rest/activity/events?apiVersion=2022-11-28)
+- [GitHub event types](https://docs.github.com/en/rest/using-the-rest-api/github-event-types?apiVersion=2022-11-28#event-object-common-properties)
 
-- Handle errors gracefully, such as invalid usernames or API failures.
-- filtering the activity by event type, displaying the activity in a more structured format
-- caching the fetched data to improve performance.
-- You can also explore other endpoints of the GitHub API to fetch additional information about the user or their repositories.
+## TODOs
+
+- [x] Handle errors gracefully, such as invalid usernames or API failures.
+- [ ] filtering the activity by event type, displaying the activity in a more structured format
+- [ ] caching the fetched data to improve performance.
+- [ ] explore other endpoints of the GitHub API to fetch additional information about the user or their repositories.
+- [ ] use classes to structure the code better.
+
+## Inspiration
 
 - [roadmap.sh](https://roadmap.sh/projects/github-user-activity)
--- main.py
+++ main.py
@@ -1,48 +1,55 @@
-"""
-
-"""
 import sys
 import json
 import http.client
 
+
 def user_name() -> str:
+    """
+    check the user name if it exist will be return
+    """
     args = sys.argv
-    if   len(args) != 2:
-        print("Usage: python main.py <username>")
+    if len(args) != 2:
+        print("Usage: python3 main.py <username>")
         sys.exit()
     return args[1]
 
-def get_latest_events(username):
-    """
-    PASS
-    """
+
+def get_latest_events(username: str) -> None:
     headers = {
     "Host": "api.github.com",
-    "User-Agent": "python-http-client"
+    "User-Agent": "python-http-client",
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28"
     }
+
     conn = http.client.HTTPSConnection("api.github.com")
     # in python3.9 > if i write the headers directly in the parameter it will give me error
     # from the server (its bug in http.client)
     conn.request("GET", f"/users/{username}/events", headers=headers)
     response = conn.getresponse()
-    data = response.read()
 
-    print(response.status, response.reason)
-    print("\nThe Headers: ")
-    print(response.headers)
-    print("\nThe data as a string:")
-    
+    data = response.read()
     dictData = json.loads(data.decode())
 
-    for num, i in enumerate(dictData, start=1):
-        print(f"The number = {num}")
+    print("\n==== GitHub Activity for USERNAME ====")
+    for x, i in enumerate(dictData, 1):
+
+        print(f"[{x}]")
         for j in i:
-            print(j)
-        print()
+            if j == "type":
+                print(f"Event: {i[j]}")
+            elif j == "repo":
+                print(f"Repo: {i[j]['name']}")
+            elif j == "created_at":
+                print(f"Date: {i[j]}")
+        print("----------------------------")
 
     conn.close()
 
 
 if __name__ == "__main__":
-    username = user_name()
-    get_latest_events(username=username)
+    try:
+        user = user_name()
+        get_latest_events(user)
+    except Exception:
+        print("API failures")
